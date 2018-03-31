cdeec71bd9308e7c1eba7c59453b76617eed212a
thmslmr <thomas.lemeur@edu.esiee.fr>
2018-03-31 14:22:28 +0200

    Lint

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 import re
 from collections import Counter
 
-class Corrector :
-	WORDS = None
 
-	def __init__(self) :
-		text = open('txt/candide.txt').read() + open('txt/ddbl.txt').read()
-		self.WORDS = Counter(self.wordsFromTxt( text ))
-		return
-	def wordsFromTxt(self, txt) :
-		return re.findall( r'\w+', txt.lower() )
+class Corrector:
 
-	def probabiltyOfWord(self, word) :
-		N = sum(self.WORDS.values())
-		return self.WORDS[word]/N
+    def __init__(self, lang='fr'):
+        text = open('txt/candide.txt').read() + open('txt/ddbl.txt').read()
+        self.words = Counter(self.words_from_txt(text))
+        return
 
-	def knownWords(self, words) :
-		return set(word for word in words if word in self.WORDS)
+    def words_from_txt(self, txt):
+        return re.findall(r'\w+', txt.lower())
 
-	def level_1_edit(self, word) :
-		letters = 'abcdefghijklmnopqrstuvwxyz'
-		splits = [ (word[:i], word[i:]) for i in range(len(word)+1) ]
-		deletes = [ left + right[1:] for left, right in splits if right]
-		transposes = [ left + right[1] + right[0] + right[2:] for left, right in splits if len(right) >1 ]
-		replaces = [ left + letter + right[1:] for left, right in splits if right for letter in letters ]
-		inserts = [left + letter + right for left, right in splits for letter in letters]
-		return set(deletes + transposes + replaces + inserts)
+    def probabilty_of_word(self, word):
+        N = sum(self.words.values())
+        return self.words[word]/N
 
-	def level_2_edit(self, word) :
-		return ( level_2 for level_1 in self.level_1_edit(word) for level_2 in self.level_1_edit(level_1) )
+    def known_words(self, words):
+        return set(word for word in words if word in self.words)
 
-	def candidates(self, word) :
-		return (self.knownWords([word]) or self.knownWords( self.level_1_edit(word) ) or self.knownWords( self.level_2_edit(word) ) or [word])
+    def level_1_edit(self, word):
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        splits = [(word[:i], word[i:]) for i in range(len(word)+1)]
+        deletes = [left + right[1:] for left, right in splits if right]
+        transposes = [left + right[1] + right[0] + right[2:] for left, right in
+                      splits if len(right) > 1]
+        replaces = [left + letter + right[1:] for left, right in splits if
+                    right for letter in letters]
+        inserts = [left + letter + right for left, right in splits for letter
+                   in letters]
 
-	def correct(self, word) :
-		return max(self.candidates(word), key=self.probabiltyOfWord)
+        return set(deletes + transposes + replaces + inserts)
+
+    def level_2_edit(self, word):
+        return (level_2 for level_1 in self.level_1_edit(word) for level_2 in
+                self.level_1_edit(level_1))
+
+    def get_candidates(self, word):
+        return (self.known_words([word]) or
+                self.known_words(self.level_1_edit(word)) or
+                self.known_words(self.level_2_edit(word)) or
+                [word])
+
+    def correct(self, word):
+        return max(self.get_candidates(word), key=self.probabilty_of_word)
